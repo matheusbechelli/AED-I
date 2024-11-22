34888fcaa185e9c87d9ea509b96a14ac24bacf6d
matheusbechelli <matheus.bechelli@unifesp.br>
2024-11-22 18:25:17 -0300

    Create 1001 - Extremamente básico.c

---
+++ 1001 - Extremamente básico.c
@@ -0,0 +1,21 @@
+/*
+ Autor: Matheus Bechelli Duarte Migliaresi;
+ RA: 180383;
+ Exercício: 1001;
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int a, b, x;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    x = a + b;
+    
+    printf("X = %d\n", x);
+    
+}
