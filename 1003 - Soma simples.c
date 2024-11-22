3694513d960758fb1de5cf09982354233af40af0
matheusbechelli <matheus.bechelli@unifesp.br>
2024-11-22 18:03:10 -0300

    Create 1003 - Soma simples.c

---
+++ 1003 - Soma simples.c
@@ -0,0 +1,23 @@
+/*
+  Autor: Matheus Bechelli Duarte Migliaresi;
+  RA: 180383;
+  Exercício: 1003;
+  Data: 22/11/2024;
+  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main() {
+     
+     int a, b, soma;
+     
+     scanf("%d", &a);
+     scanf("%d", &b);
+     
+     soma = a + b;
+     
+     printf("SOMA = %d\n", soma);
+     
+ }
+
