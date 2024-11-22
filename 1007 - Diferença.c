6b995b79ea5c53762572c7070e3191d96d588f78
matheusbechelli <matheus.bechelli@unifesp.br>
2024-11-22 18:28:45 -0300

    Create 1007 - Diferença.c

---
+++ 1007 - Diferença.c
@@ -0,0 +1,23 @@
+/*
+ Autor: Matheus Bechelli Duarte Migliaresi;
+ RA: 180383;
+ Exercício: 1007;
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int a, b, c, d, x;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+    
+    x = (a * b - c * d);
+    
+    printf("DIFERENCA = %d\n", x);
+    
+}
