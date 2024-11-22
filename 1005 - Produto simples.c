8bc93105b70cba704766713c30f6ea3ee00d5356
matheusbechelli <matheus.bechelli@unifesp.br>
2024-11-22 18:05:52 -0300

    1005 - Produto simples.c

---
+++ 1005 - Produto simples.c
@@ -0,0 +1,23 @@
+/*
+ Autor: Matheus Bechelli Duarte Migliaresi;
+ RA: 180383;
+ Exercício: 1004;
+ Data: 22/11/2024;
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int a, b, prod;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    prod = a * b;
+    
+    printf("PROD = %d\n", prod);
+    
+}
+
