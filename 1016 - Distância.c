b341b6c06fe43402bed7ccb4e67ac4ce24aa821d
matheusbechelli <matheus.bechelli@unifesp.br>
2024-11-23 01:09:51 -0300

    Create 1016 - Distância.c

---
+++ 1016 - Distância.c
@@ -0,0 +1,21 @@
+/*
+ Autor: Matheus Bechelli Duarte Migliaresi;
+ RA: 180383;
+ Exercício: 1016;
+ Data: 23/11/2024;
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    
+    int distancia, tempo;
+    
+    scanf("%d", &distancia);
+    
+    tempo = 2 * distancia;
+    
+    printf("%d minutos\n", tempo);
+}
+            
